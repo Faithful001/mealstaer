2e510cb9b0116805dfa3ea4cdddf7ff382a816c8
Faithful <faithfuleromosele001@gmail.com>
2023-11-16 22:32:49 +0100

    Set up sessions for prod

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,9 @@ import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
 import passport from "passport";
-import session from "express-session";
+// import session from "express-session";
 import cookieParser from "cookie-parser";
-const MongoDBStore = require("connect-mongodb-session")(session);
+// const MongoDBStore = require("connect-mongodb-session")(session);
 import cookieSession from "cookie-session";
 const port = process.env.PORT || 8080;
 const mealView = require("./views/mealView");
@@ -22,44 +22,41 @@ const app = express();
 // middleware
 const twoDaysInMilliseconds = 2 * 24 * 60 * 60 * 1000;
 
-const store = new MongoDBStore({
-	uri: process.env.MONGO_URI, // Replace with your MongoDB URI
-	collection: "sessions",
-	expires: twoDaysInMilliseconds,
-	// connectionOptions: {
-	// 	useNewUrlParser: true,
-	// 	useUnifiedTopology: true,
-	// },
-});
+// const store = new MongoDBStore({
+// 	uri: process.env.MONGO_URI, // Replace with your MongoDB URI
+// 	collection: "sessions",
+// 	expires: twoDaysInMilliseconds,
+// 	// connectionOptions: {
+// 	// 	useNewUrlParser: true,
+// 	// 	useUnifiedTopology: true,
+// 	// },
+// });
 
-store.on("error", (error) => {
-	console.error("Session Store Error:", error);
-});
-
-app.use(
-	session({
-		secret: process.env.COOKIE_KEY,
-		resave: false,
-		saveUninitialized: true,
-		store: store,
-		cookie: {
-			maxAge: twoDaysInMilliseconds,
-			expires: new Date(Date.now() + twoDaysInMilliseconds),
-		},
-	})
-);
+// store.on("error", (error) => {
+// 	console.error("Session Store Error:", error);
+// });
 
 // app.use(
-// 	cookieSession({
-// 		name: "session",
-// 		keys: [process.env.COOKIE_KEY],
-// 		maxAge: twoDaysInMilliseconds,
+// 	session({
+// 		secret: process.env.COOKIE_KEY,
+// 		resave: false,
+// 		saveUninitialized: true,
+// 		store: store,
 // 		cookie: {
-// 			secure: true,
-// 			// other cookie options...
+// 			maxAge: twoDaysInMilliseconds,
+// 			expires: new Date(Date.now() + twoDaysInMilliseconds),
 // 		},
 // 	})
 // );
+
+app.use(
+	cookieSession({
+		name: "session",
+		keys: [process.env.COOKIE_KEY],
+		maxAge: twoDaysInMilliseconds,
+		expires: new Date(Date.now() + twoDaysInMilliseconds),
+	})
+);
 
 app.use(cookieParser());
 

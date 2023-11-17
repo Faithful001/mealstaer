78d22070140b37d7821ab4772497de6a6a231975
Faithful <faithfuleromosele001@gmail.com>
2023-11-17 14:36:55 +0100

    finishing touches

--- frontend/src/utils/methods/url/URL.tsx
+++ frontend/src/utils/methods/url/URL.tsx
@@ -1,4 +1,4 @@
 export const URL = {
-	prodURL: "http://localhost:4000",
-	// prodURL: "https://mealstaer.onrender.com",
+	localURL: "http://localhost:4000",
+	prodURL: "https://mealstaer.onrender.com",
 };

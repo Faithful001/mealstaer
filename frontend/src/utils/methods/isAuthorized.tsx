87371b02aefd99beabd7ad035b7c4e33c25c59d3
Faithful <faithfuleromosele001@gmail.com>
2024-01-09 15:13:07 +0100

    middleware

--- frontend/src/utils/methods/isAuthorized.tsx
+++ frontend/src/utils/methods/isAuthorized.tsx
@@ -3,7 +3,7 @@ function isAuthorized() {
 	for (let i = 0; i < cookies.length; i++) {
 		const cookie = cookies[i];
 		if (cookie.startsWith("connect.sid=")) {
-			cookie.split("=")[1];
+			return cookie.split("=")[1];
 		}
 	}
 }

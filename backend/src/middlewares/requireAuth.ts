18fb18df1918e2e0ae4ba166dd7e1ca07abc96aa
faithfultheking <83516086+Faithful001@users.noreply.github.com>
2025-09-26 04:00:03 +0100

    fixed the jwt util

--- backend/src/middlewares/requireAuth.ts
+++ backend/src/middlewares/requireAuth.ts
@@ -1,23 +1,23 @@
-import jwt from "jsonwebtoken";
+import jwtUtil from "../utils/jwt.util";
 import { Request, Response, NextFunction } from "express"; // Import Request, Response, and NextFunction types
 const User = require("../models/userModel");
 
 const requireAuth = async (req: Request, res: Response, next: NextFunction) => {
-	const { authorization } = req.headers;
+  const { authorization } = req.headers;
 
-	if (!authorization) {
-		return res.status(401).json({ error: "Authorization token required" });
-	}
-	const token = authorization.split(" ")[1];
+  if (!authorization) {
+    return res.status(401).json({ error: "Authorization token required" });
+  }
+  const token = authorization.split(" ")[1];
 
-	try {
-		const { _id } = jwt.verify(token, process.env.JWT_SEC);
-		req.user = await User.findOne({ _id }).select("_id");
-		next();
-	} catch (error) {
-		console.log(error);
-		res.status(401).json({ error: "Request is not authorized" });
-	}
+  try {
+    const { _id } = jwtUtil.verifyToken(token);
+    req.user = await User.findOne({ _id }).select("_id");
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ error: "Request is not authorized" });
+  }
 };
 
 module.exports = requireAuth;
--- backend/src/middlewares/verifyJwt.ts
+++ backend/src/middlewares/verifyJwt.ts
@@ -1,16 +1,17 @@
-import jwt from "jsonwebtoken";
+import jwtUtil from "../utils/jwt.util";
+import { Request, Response, NextFunction } from "express";
 const User = require("../models/userModel");
 
-const verifyJwt = async (req, res, next) => {
-	const { token } = req.cookie;
-	try {
-		const { _id } = jwt.verify(token, process.env.JWT_SEC);
-		req.user = await User.findOne({ _id }).select("_id");
-		next();
-	} catch (error) {
-		res.clearCookie("token");
-		res.status(401).json({ error: "You are not authorized " + error });
-	}
+const verifyJwt = async (req: Request, res: Response, next: NextFunction) => {
+  const { token } = req.cookies;
+  try {
+    const { _id } = jwtUtil.verifyToken(token);
+    req.user = await User.findOne({ _id }).select("_id");
+    next();
+  } catch (error) {
+    res.clearCookie("token");
+    res.status(401).json({ error: "You are not authorized " + error });
+  }
 };
 
 module.exports = verifyJwt;
